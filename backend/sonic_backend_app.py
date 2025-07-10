36e23c396d35e7a9b61f79c6bdf71b37f30dac66
BubbaDiego <106400961+BubbaDiego@users.noreply.github.com>
2025-07-09 17:12:47 -0700

    Add db admin read_table API

--- backend/sonic_backend_app.py
+++ backend/sonic_backend_app.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Adds the parent of 'backend' to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,6 +16,7 @@ from backend.routes.alert_thresholds_api import (
     alerts_router,
 )
 from backend.routes.monitor_status_api import router as monitor_status_router
+from backend.routes.db_admin_api import router as db_admin_router
 
 app = FastAPI(title="Sonic API")
 
@@ -25,7 +26,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3006",  # Clearly match your frontend app origin
         "http://localhost:3000",
-        "*"  # Or "*" to allow all clearly (simpler during development)
+        "*",  # Or "*" to allow all clearly (simpler during development)
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -39,12 +40,16 @@ app.include_router(wallet_router)
 app.include_router(traders_router)
 app.include_router(threshold_router)
 app.include_router(monitor_status_router)
+app.include_router(db_admin_router)
 app.include_router(alerts_router)
+
 
 @app.get("/api/status")
 async def status():
     return {"status": "FastAPI backend online 🚀"}
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("backend.sonic_backend_app:app", host="0.0.0.0", port=5000, reload=True)
