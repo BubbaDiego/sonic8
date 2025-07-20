5703676f4124399499788ee2851c63f62da4eb02
BubbaDiego <106400961+BubbaDiego@users.noreply.github.com>
2025-07-19 20:45:58 -0700

    chore(frontend): add TypeScript support

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import jsconfigPaths from 'vite-jsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // ==============================|| Vite Config (patched) ||============================== //
 export default defineConfig(({ mode }) => {
@@ -42,9 +42,10 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs'
-      }
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
     base: BASE_NAME,
-    plugins: [react(), jsconfigPaths()]
+    plugins: [react(), tsconfigPaths()]
   };
 });
